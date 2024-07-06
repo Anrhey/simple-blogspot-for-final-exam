7ac575745848ec28a3234424cf569a6b4fd30286
Dreyyy <117262973+Anrhey@users.noreply.github.com>
2024-07-07 02:02:50 +0800

    added confirmation messages

--- src/app/(components)/Login/Login.jsx
+++ src/app/(components)/Login/Login.jsx
@@ -8,6 +8,8 @@ const Login = () => {
     email: "",
     password: "",
   });
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
 
   const handleChange = (e) => {
@@ -31,17 +33,20 @@ const Login = () => {
         }
       );
 
-      const data = await response.json(); // Ensure JSON parsing
-      const { token } = data; // Destructure token from response data
+      const data = await response.json();
+      const { token } = data;
 
       if (token) {
-        localStorage.setItem("token", token); // Store token in localStorage
-        console.log("Token:", token);
-        router.push("/homepage");
+        localStorage.setItem("token", token);
+        setSuccessMessage("Login successful! Redirecting to homepage...");
+        setTimeout(() => {
+          router.push("/homepage");
+        }, 3000); // Redirect after 3 seconds
       } else {
-        console.error("No token received");
+        setErrorMessage("Login failed. Please check your email and password.");
       }
     } catch (error) {
+      setErrorMessage("Login failed. Please try again later.");
       console.error("Login failed:", error);
     }
   };
@@ -52,7 +57,7 @@ const Login = () => {
         <h2 className={styles.title}>Login</h2>
         <form onSubmit={handleLogin}>
           <div className={styles.inputGroup}>
-            <label for="email" className={styles.label}>
+            <label htmlFor="email" className={styles.label}>
               Email:
             </label>
             <input
@@ -65,7 +70,7 @@ const Login = () => {
             />
           </div>
           <div className={styles.inputGroup}>
-            <label for="password" className={styles.label}>
+            <label htmlFor="password" className={styles.label}>
               Password:
             </label>
             <input
@@ -81,6 +86,10 @@ const Login = () => {
             Login
           </button>
         </form>
+        {successMessage && (
+          <p className={styles.successMessage}>{successMessage}</p>
+        )}
+        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
         <p className={styles.signup}>
           Don{"'"}t have an account?{" "}
           <a href="/RegistrationPage" className={styles.signupLink}>
@@ -93,3 +102,99 @@ const Login = () => {
 };
 
 export default Login;
+
+// "use client";
+// import React, { useState } from "react";
+// import { useRouter } from "next/navigation";
+// import styles from "./styles.module.css";
+
+// const Login = () => {
+//   const [formData, setFormData] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   const router = useRouter();
+
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch(
+//         `${process.env.NEXT_PUBLIC_API_URL}/api/login`,
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-type": "application/json",
+//           },
+//           body: JSON.stringify(formData),
+//         }
+//       );
+
+//       const data = await response.json(); // Ensure JSON parsing
+//       const { token } = data; // Destructure token from response data
+
+//       if (token) {
+//         localStorage.setItem("token", token); // Store token in localStorage
+//         console.log("Token:", token);
+//         router.push("/homepage");
+//       } else {
+//         console.error("No token received");
+//       }
+//     } catch (error) {
+//       console.error("Login failed:", error);
+//     }
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.loginBox}>
+//         <h2 className={styles.title}>Login</h2>
+//         <form onSubmit={handleLogin}>
+//           <div className={styles.inputGroup}>
+//             <label for="email" className={styles.label}>
+//               Email:
+//             </label>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               onChange={handleChange}
+//               className={styles.input}
+//               placeholder="Email"
+//             />
+//           </div>
+//           <div className={styles.inputGroup}>
+//             <label for="password" className={styles.label}>
+//               Password:
+//             </label>
+//             <input
+//               type="password"
+//               id="password"
+//               name="password"
+//               onChange={handleChange}
+//               className={styles.input}
+//               placeholder="Password"
+//             />
+//           </div>
+//           <button type="submit" className={styles.loginButton}>
+//             Login
+//           </button>
+//         </form>
+//         <p className={styles.signup}>
+//           Don{"'"}t have an account?{" "}
+//           <a href="/RegistrationPage" className={styles.signupLink}>
+//             Sign up here
+//           </a>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
--- src/app/(components)/Profile/ProfileFeed.jsx
+++ src/app/(components)/Profile/ProfileFeed.jsx
@@ -12,6 +12,8 @@ import {
   Divider,
   CircularProgress,
   Tooltip,
+  Snackbar,
+  Alert,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -24,6 +26,7 @@ import Link from "next/link";
 const ProfileFeed = () => {
   const queryClient = useQueryClient();
   const [deletingPostId, setDeletingPostId] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(false);
 
   const token =
     typeof window !== "undefined" ? localStorage.getItem("token") : null;
@@ -38,6 +41,7 @@ const ProfileFeed = () => {
     onSuccess: () => {
       queryClient.invalidateQueries(["user"]);
       setDeletingPostId(null); // Reset the deleting post ID
+      setSuccessMessage(true); // Show success message
     },
     onError: () => {
       setDeletingPostId(null); // Reset the deleting post ID in case of error
@@ -84,46 +88,43 @@ const ProfileFeed = () => {
               .reverse()
               .map((post, index) => (
                 <Paper key={index} sx={{ p: 2, mb: 2, position: "relative" }}>
-                  <Link href={`/edit-post/${post.postId}`}>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                      }}
-                    >
-                      <Typography variant="h6">{post.title}</Typography>
-                      <Box>
-                        <Tooltip title="Edit">
-                          <IconButton
-                            color="primary"
-                            href={`/edit-post/${post.postId}`}
-                            onClick={() => console.log(post.postId)}
-                          >
-                            <EditIcon />
-                          </IconButton>
-                        </Tooltip>
-                        <Tooltip title="Delete">
-                          <IconButton
-                            color="secondary"
-                            onClick={() => handleDelete(post.postId)}
-                          >
-                            <DeleteIcon />
-                          </IconButton>
-                        </Tooltip>
-                      </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography variant="h6">{post.title}</Typography>
+                    <Box>
+                      <Tooltip title="Edit">
+                        <IconButton
+                          color="primary"
+                          href={`/edit-post/${post.postId}`}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Delete">
+                        <IconButton
+                          color="secondary"
+                          onClick={() => handleDelete(post.postId)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </Tooltip>
                     </Box>
-                    <Typography variant="body1">{post.content}</Typography>
-                    {post.imageUrl.length ? (
-                      <Box sx={{ mt: 2 }}>
-                        <img
-                          src={post.imageUrl}
-                          alt="image"
-                          style={{ maxWidth: "100%", borderRadius: 4 }}
-                        />
-                      </Box>
-                    ) : null}
-                  </Link>
+                  </Box>
+                  <Typography variant="body1">{post.content}</Typography>
+                  {post.imageUrl.length ? (
+                    <Box sx={{ mt: 2 }}>
+                      <img
+                        src={post.imageUrl}
+                        alt="image"
+                        style={{ maxWidth: "100%", borderRadius: 4 }}
+                      />
+                    </Box>
+                  ) : null}
                   <Divider sx={{ my: 2 }} />
                   <Box
                     sx={{
@@ -160,6 +161,19 @@ const ProfileFeed = () => {
           </Box>
         </Grid>
       </Grid>
+      <Snackbar
+        open={successMessage}
+        autoHideDuration={6000}
+        onClose={() => setSuccessMessage(false)}
+      >
+        <Alert
+          onClose={() => setSuccessMessage(false)}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Post deleted successfully!
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
@@ -179,6 +193,7 @@ export default ProfileFeed;
 //   Grid,
 //   Divider,
 //   CircularProgress,
+//   Tooltip,
 // } from "@mui/material";
 // import DeleteIcon from "@mui/icons-material/Delete";
 // import EditIcon from "@mui/icons-material/Edit";
@@ -186,6 +201,7 @@ export default ProfileFeed;
 // import ProfileCard from "../Profile/ProfileCard";
 // import { fetchUser } from "./actions";
 // import { deletePost } from "../EditPost/actions";
+// import Link from "next/link";
 
 // const ProfileFeed = () => {
 //   const queryClient = useQueryClient();
@@ -215,8 +231,8 @@ export default ProfileFeed;
 //     mutation.mutate(postId);
 //   };
 
-//   if (isLoading) return <h2>Loading...</h2>;
-//   if (isError) return <h2>Error loading posts</h2>;
+//   if (isLoading) return <CircularProgress />;
+//   if (isError) return <Typography>Error loading posts</Typography>;
 
 //   return (
 //     <Container sx={{ mt: 4 }}>
@@ -250,40 +266,46 @@ export default ProfileFeed;
 //               .reverse()
 //               .map((post, index) => (
 //                 <Paper key={index} sx={{ p: 2, mb: 2, position: "relative" }}>
-//                   <Box
-//                     sx={{
-//                       display: "flex",
-//                       justifyContent: "space-between",
-//                       alignItems: "center",
-//                     }}
-//                   >
-//                     <Typography variant="h6">{post.title}</Typography>
-//                     <Box>
-//                       <IconButton
-//                         color="primary"
-//                         href={`/edit-post/${post.postId}`}
-//                         onClick={() => console.log(post.postId)}
-//                       >
-//                         <EditIcon />
-//                       </IconButton>
-//                       <IconButton
-//                         color="secondary"
-//                         onClick={() => handleDelete(post.postId)}
-//                       >
-//                         <DeleteIcon />
-//                       </IconButton>
+//                   <Link href={`/edit-post/${post.postId}`}>
+//                     <Box
+//                       sx={{
+//                         display: "flex",
+//                         justifyContent: "space-between",
+//                         alignItems: "center",
+//                       }}
+//                     >
+//                       <Typography variant="h6">{post.title}</Typography>
+//                       <Box>
+//                         <Tooltip title="Edit">
+//                           <IconButton
+//                             color="primary"
+//                             href={`/edit-post/${post.postId}`}
+//                             onClick={() => console.log(post.postId)}
+//                           >
+//                             <EditIcon />
+//                           </IconButton>
+//                         </Tooltip>
+//                         <Tooltip title="Delete">
+//                           <IconButton
+//                             color="secondary"
+//                             onClick={() => handleDelete(post.postId)}
+//                           >
+//                             <DeleteIcon />
+//                           </IconButton>
+//                         </Tooltip>
+//                       </Box>
 //                     </Box>
-//                   </Box>
-//                   <Typography variant="body1">{post.content}</Typography>
-//                   {post.imageUrl.length ? (
-//                     <Box sx={{ mt: 2 }}>
-//                       <img
-//                         src={post.imageUrl}
-//                         alt="image"
-//                         style={{ maxWidth: "100%", borderRadius: 4 }}
-//                       />
-//                     </Box>
-//                   ) : null}
+//                     <Typography variant="body1">{post.content}</Typography>
+//                     {post.imageUrl.length ? (
+//                       <Box sx={{ mt: 2 }}>
+//                         <img
+//                           src={post.imageUrl}
+//                           alt="image"
+//                           style={{ maxWidth: "100%", borderRadius: 4 }}
+//                         />
+//                       </Box>
+//                     ) : null}
+//                   </Link>
 //                   <Divider sx={{ my: 2 }} />
 //                   <Box
 //                     sx={{
@@ -291,8 +313,12 @@ export default ProfileFeed;
 //                       justifyContent: "space-between",
 //                     }}
 //                   >
-//                     <Typography variant="body2">Likes here</Typography>
-//                     <Typography variant="body2">Comments here</Typography>
+//                     <Typography variant="body2">
+//                       {post.likes.length} Likes
+//                     </Typography>
+//                     <Typography variant="body2">
+//                       {post.comments.length} Comments
+//                     </Typography>
 //                   </Box>
 //                   {deletingPostId === post.postId && (
 //                     <Box
@@ -321,185 +347,3 @@ export default ProfileFeed;
 // };
 
 // export default ProfileFeed;
-
-// // "use client";
-
-// // import React, { useEffect, useState } from "react";
-// // import {
-// //   Box,
-// //   Container,
-// //   IconButton,
-// //   InputBase,
-// //   Paper,
-// //   Typography,
-// //   Grid,
-// //   Avatar,
-// //   Divider,
-// //   Button,
-// //   CircularProgress,
-// // } from "@mui/material";
-// // import DeleteIcon from "@mui/icons-material/Delete";
-// // import EditIcon from "@mui/icons-material/Edit";
-// // import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-// // import ProfileCard from "../Profile/ProfileCard"; // Assume this is your previously created ProfileCard component
-// // import { deletePost } from "../EditPost/actions";
-
-// // const ProfileFeed = () => {
-// //   //   const [token, setToken] = useState(null);
-
-// //   //   useEffect(() => {
-// //   //     if (typeof window !== "undefined") {
-// //   //       setToken(localStorage.getItem("token"));
-// //   //     }
-// //   //   }, []);
-
-// //   const queryClient = useQueryClient();
-
-// //   const token = localStorage.getItem("token");
-
-// //   const { data, isLoading, isError } = useQuery({
-// //     queryKey: ["user"],
-// //     queryFn: () => fetchUser(token),
-// //   });
-
-// //   const mutation = useMutation({
-// //     mutationFn: (id) => deletePost(token, id),
-// //     onSuccess: () => {
-// //       queryClient.invalidateQueries(["user"]);
-// //     },
-// //   });
-
-// //   const handleDelete = async (postId) => {
-// //     mutation.mutate(postId);
-// //   };
-
-// //   if (isLoading) return <h2>Loading...</h2>;
-
-// //   return (
-// //     <>
-// //       <Container sx={{ mt: 4 }}>
-// //         <Grid container spacing={4}>
-// //           {/* Profile Section */}
-// //           <Grid item xs={12} md={4}>
-// //             <ProfileCard />
-// //           </Grid>
-
-// //           {/* Main Content Section */}
-// //           <Grid item xs={12} md={8}>
-// //             <Paper
-// //               component="form"
-// //               sx={{
-// //                 p: "2px 4px",
-// //                 display: "flex",
-// //                 alignItems: "center",
-// //                 mb: 4,
-// //               }}
-// //             >
-// //               <InputBase
-// //                 sx={{ ml: 1, flex: 1 }}
-// //                 placeholder="Search for post..."
-// //               />
-// //             </Paper>
-
-// //             {/* Blog Posts */}
-// //             <Box sx={{ mt: 4 }}>
-// //               {mutation.isPending ? (
-// //                 <Box
-// //                   display="flex"
-// //                   justifyContent="center"
-// //                   alignItems="center"
-// //                   sx={{ mt: 4 }}
-// //                 >
-// //                   <CircularProgress />
-// //                 </Box>
-// //               ) : (
-// //                 <Box sx={{ mt: 4 }}>
-// //                   {data?.userData?.posts
-// //                     .slice()
-// //                     .reverse()
-// //                     .map((post, index) => (
-// //                       <Paper key={index} sx={{ p: 2, mb: 2 }}>
-// //                         <Box
-// //                           sx={{
-// //                             display: "flex",
-// //                             justifyContent: "space-between",
-// //                             alignItems: "center",
-// //                           }}
-// //                         >
-// //                           <Typography variant="h6">{post.title}</Typography>
-// //                           <Box>
-// //                             <IconButton
-// //                               color="primary"
-// //                               href={`/edit-post/${post.postId}`}
-// //                               onClick={() => console.log(post.postId)}
-// //                             >
-// //                               <EditIcon />
-// //                             </IconButton>
-// //                             <IconButton
-// //                               color="secondary"
-// //                               onClick={() => handleDelete(post.postId)}
-// //                             >
-// //                               <DeleteIcon />
-// //                             </IconButton>
-// //                           </Box>
-// //                         </Box>
-// //                         <Typography variant="body1">{post.content}</Typography>
-// //                         {post.imageUrl.length ? (
-// //                           <Box sx={{ mt: 2 }}>
-// //                             <img
-// //                               src={post.imageUrl}
-// //                               alt="image"
-// //                               style={{ maxWidth: "100%", borderRadius: 4 }}
-// //                             />
-// //                           </Box>
-// //                         ) : null}
-// //                         <Divider sx={{ my: 2 }} />
-// //                         <Box
-// //                           sx={{
-// //                             display: "flex",
-// //                             justifyContent: "space-between",
-// //                           }}
-// //                         >
-// //                           <Typography variant="body2">Likes here</Typography>
-// //                           <Typography variant="body2">Comments here</Typography>
-// //                         </Box>
-// //                       </Paper>
-// //                     ))}
-// //                 </Box>
-// //               )}
-// //             </Box>
-// //             {/* <Box sx={{ mt: 4 }}>
-// //               {data.userData.posts
-// //                 .slice()
-// //                 .reverse()
-// //                 .map((post, index) => (
-// //                   <Paper key={index} sx={{ p: 2, mb: 2 }}>
-// //                     <Typography variant="h6">{post.title}</Typography>
-// //                     <Typography variant="body1">{post.content}</Typography>
-// //                     {post.imageUrl.length ? (
-// //                       <Box sx={{ mt: 2 }}>
-// //                         <img
-// //                           src={post.imageUrl}
-// //                           alt="image"
-// //                           style={{ maxWidth: "100%", borderRadius: 4 }}
-// //                         />
-// //                       </Box>
-// //                     ) : null}
-// //                     <Divider sx={{ my: 2 }} />
-// //                     <Box
-// //                       sx={{ display: "flex", justifyContent: "space-between" }}
-// //                     >
-// //                       <Typography variant="body2">Likes here</Typography>
-// //                       <Typography variant="body2">Comments here</Typography>
-// //                     </Box>
-// //                   </Paper>
-// //                 ))}
-// //             </Box> */}
-// //           </Grid>
-// //         </Grid>
-// //       </Container>
-// //     </>
-// //   );
-// // };
-
-// // export default ProfileFeed;
