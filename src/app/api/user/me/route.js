dcdab0f92369070920824a2628d8cf97be0cbef2
Dreyyy <117262973+Anrhey@users.noreply.github.com>
2024-07-07 17:07:31 +0800

    fixing error

--- src/app/api/user/me/route.js
+++ src/app/api/user/me/route.js
@@ -8,23 +8,27 @@ const handler = async (req) => {
   const userID = req.user.id;
 
   //Example usage of Prisma client
-  const userData = await prisma.user.findUnique({
-    where: { id: userID },
-    select: {
-      id: true,
-      email: true,
-      name: true,
-      profileImage: true,
-      posts: {
-        include: {
-          likes: true,
-          comments: true,
+  try {
+    const userData = await prisma.user.findUnique({
+      where: { id: userID },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        profileImage: true,
+        posts: {
+          include: {
+            likes: true,
+            comments: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  return NextResponse.json({ status: 200, userData });
+    return NextResponse.json({ status: 200, userData });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 };
 
 export const GET = authMiddleware(handler);
