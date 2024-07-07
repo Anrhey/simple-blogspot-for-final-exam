a5f5f52282cca329dafaf1444875f39145937ed6
Dreyyy <117262973+Anrhey@users.noreply.github.com>
2024-07-07 12:17:33 +0800

    try to disable cors

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -22,9 +22,22 @@ const allowedOrigins = [
 export async function POST(req) {
   //await cors(req, NextResponse); // Run the middleware
   //await runMiddleware(req, NextResponse, cors);
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+  // Set CORS headers to allow all origins
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
 
   const { email, password } = await req.json();
