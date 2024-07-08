cacb07302cd2cd121c61cc6de00b0a9c633088da
Dreyyy <117262973+Anrhey@users.noreply.github.com>
2024-07-08 17:12:03 +0800

    fix fetch user

--- src/app/(components)/Profile/actions.js
+++ src/app/(components)/Profile/actions.js
@@ -3,7 +3,6 @@ export async function fetchUser(token) {
     const res = await fetch("/api/user/me", {
       method: "GET",
       headers: {
-        "Content-type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     });
